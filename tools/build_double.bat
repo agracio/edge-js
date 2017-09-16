ff4d1aa542abbf3bb98a79a28ab7a3ed0a408f29
agracio <antonas.graciovas@outlook.com>
2017-09-16 13:03:19 +0100

    updating build

--- tools/build_double.bat
+++ tools/build_double.bat
@@ -102,7 +102,7 @@ set GYP=%APPDATA%\npm\node_modules\node-gyp\bin\node-gyp.js
 pushd "%SELF%\.."
 
 "%NODEEXE%" "%GYP%" configure --msvs_version=2015
-"%SELF%\build\repl.exe" ./build/edge_nativeclr.vcxproj "%USERPROFILE%\.node-gyp\%1\$(Configuration)\node.lib" "%SELF%\build\node-%1-%2\node.lib"
+"%SELF%\build\repl.exe" ./build/edge_nativeclr.vcxproj "%USERPROFILE%\.node-gyp\%1\%2\node.lib" "%SELF%build\node-%1-%2\node.lib"
 "%NODEEXE%" "%GYP%" build
 mkdir "%SELF%\build\nuget\content\edge\%2" > nul 2>&1
 copy /y build\release\edge_nativeclr.node "%SELF%\build\nuget\content\edge\%2"
