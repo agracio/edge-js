24b27fef19f87c5e6c62b8f2e08064ea354398bf
agracio <antonas.graciovas@outlook.com>
2017-12-31 17:04:09 +0000

    updating typescript definitions

--- edge-js.d.ts
+++ edge-js.d.ts
@@ -1,5 +1,5 @@
 declare module 'edge-js' {
-    function func<TInput, TOutput>(params: string | Function | Params | Source): Func<TInput, TOutput>
+    function func<TInput, TOutput>(language: string = 'cs', params: string | Function | Params | Source): Func<TInput, TOutput>
     interface Params {
         assemblyFile: string
         typeName?: string
