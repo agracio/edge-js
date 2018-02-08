034329aaa103eeb902234f00f148100ee1859656
mtajmajer <Michael.Tajmajer@Emerson.com>
2018-02-08 14:54:56 -0600

    Fix load error when linked folder is used.

--- src/double/Edge.js/dotnet/EdgeJs.cs
+++ src/double/Edge.js/dotnet/EdgeJs.cs
@@ -7,6 +7,9 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
+#if DOTNETVERSION
+[assembly:AssemblyVersionAttribute("8.9.3.0")]
+#endif
 namespace EdgeJs
 {
     public class Edge
@@ -15,6 +18,13 @@ namespace EdgeJs
         static bool initialized;
         static Func<object, Task<object>> compileFunc;
         static ManualResetEvent waitHandle = new ManualResetEvent(false);
+        private static string edgeDirectory;
+
+        static Edge()
+        {
+            // needed because we *must* load the edgedll from the original location else we'll freeze up.
+            edgeDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(Edge)).Location);
+        }
 
         static string assemblyDirectory;
         static string AssemblyDirectory
@@ -36,6 +46,12 @@ namespace EdgeJs
                 return assemblyDirectory;
             }
         }
+
+        // in case we want to set this path and not use an enviroment var
+        public static void SetAssemblyDirectory(string folder)
+	    {
+			assemblyDirectory = folder;
+	    }
 
         [EditorBrowsable(EditorBrowsableState.Never)]
         public Task<object> InitializeInternal(object input)
@@ -81,7 +97,7 @@ namespace EdgeJs
                                 "Unsupported architecture. Only x86 and x64 are supported.");
                         }
 
-                        Thread v8Thread = new Thread(() => 
+                        Thread v8Thread = new Thread(() =>
                         {
                             List<string> argv = new List<string>();
                             argv.Add("node");
@@ -93,7 +109,8 @@ namespace EdgeJs
                                     argv.Add(p);
                                 }
                             }
-                            argv.Add(AssemblyDirectory + "\\edge\\double_edge.js");
+                            argv.Add(Path.Combine(AssemblyDirectory, "edge", "double_edge.js"));
+                            argv.Add($"-EdgeJs:{Path.Combine(edgeDirectory, "EdgeJs.dll")}");
                             nodeStart(argv.Count, argv.ToArray());
                             waitHandle.Set();
                         });
