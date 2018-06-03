6c57f4c3bc9e49b8eec049825d81fbe6d4054eac
agracio <antonas.graciovas@outlook.com>
2018-06-03 20:06:03 +0100

    updating node versions

--- test/testall.bat
+++ test/testall.bat
@@ -10,17 +10,17 @@ if "%1"=="" set run64=Y
 if "%1"=="x64" set run64=Y
 
 if "%run32%"=="Y" (
-	call "%SELF%\test.bat" ia32 10.1.0
+	call "%SELF%\test.bat" ia32 10.3.0
 	call "%SELF%\test.bat" ia32 9.11.1
-	call "%SELF%\test.bat" ia32 8.11.1
+	call "%SELF%\test.bat" ia32 8.11.2
 	call "%SELF%\test.bat" ia32 7.10.1
 	call "%SELF%\test.bat" ia32 6.14.2
 )
 
 if "%run64%"=="Y" (
-	call "%SELF%\test.bat" x64 10.1.0
+	call "%SELF%\test.bat" x64 10.3.0
 	call "%SELF%\test.bat" x64 9.11.1
-	call "%SELF%\test.bat" x64 8.11.1
+	call "%SELF%\test.bat" x64 8.11.2
 	call "%SELF%\test.bat" x64 7.10.1
 	call "%SELF%\test.bat" x64 6.14.2
 )
--- tools/buildall.bat
+++ tools/buildall.bat
@@ -1,2 +1,2 @@
 @echo off
-"%~dp0\build.bat" release 6.14.2 7.10.1 8.11.1 9.11.1 10.1.0
+"%~dp0\build.bat" release 6.14.2 7.10.1 8.11.2 9.11.1 10.3.0
