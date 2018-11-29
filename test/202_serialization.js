e2a1182c1c2fb54c80a8cb78c5e359f6b812b5cb
agracio <antonas.graciovas@gmail.com>
2018-11-29 14:01:38 +0000

    testing Appveyor Linux CI, fixing test

--- test/202_serialization.js
+++ test/202_serialization.js
@@ -29,8 +29,8 @@ describe('serialization', function () {
             func("JavaScript", function (error, result) {
                 var exception = error.toString();
                 var contains = exception.indexOf('A network-related or instance-specific error occurred while establishing a connection to SQL Server') !== -1
-                    || exception.indexOf('Server does not exist or connection refused') !== -1;
-                console.log(exception);
+                    || exception.indexOf('Server does not exist or connection refused') !== -1
+                    || exception.indexOf('System.Data.SqlClient.SqlException') !== -1;
                 assert.ok(contains);
 
                 done();
