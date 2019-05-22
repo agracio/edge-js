bfa7c2269fd0322ca8958edc4fa8af3e38a1bd0e
agracio <antonas.graciovas@gmail.com>
2019-05-22 21:34:24 +0100

    update packages, merge PR

--- tools/build.bat
+++ tools/build.bat
@@ -48,7 +48,8 @@ if %ERRORLEVEL% neq 0 (
 
 echo Building edge.node %FLAVOR% for node.js %2 v%3
 set NODEEXE=%DESTDIR%\node.exe
-set GYP=%APPDATA%\npm\node_modules\node-gyp\bin\node-gyp.js
+FOR /F "tokens=* USEBACKQ" %%F IN (`npm config get prefix`) DO (SET NODEBASE=%%F)
+set GYP=%NODEBASE%\node_modules\node-gyp\bin\node-gyp.js
 if not exist "%GYP%" (
     echo Cannot find node-gyp at %GYP%. Make sure to install with npm install node-gyp -g
     exit /b -1
