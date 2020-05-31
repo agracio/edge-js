237c30cfefc5ef027e9ec36cf806b42e975681d4
agracio <antonas.graciovas@gmail.com>
2020-05-31 17:03:34 +0100

    adding support for NodeJs 14.x

--- test/testall.bat
+++ test/testall.bat
@@ -10,6 +10,7 @@ if "%1"=="" set run64=Y
 if "%1"=="x64" set run64=Y
 
 if "%run32%"=="Y" (
+	call "%SELF%\test.bat" ia32 14.3.0
 	call "%SELF%\test.bat" ia32 13.0.1
 	call "%SELF%\test.bat" ia32 12.13.0
 	call "%SELF%\test.bat" ia32 11.3.0
@@ -19,6 +20,7 @@ if "%run32%"=="Y" (
 )
 
 if "%run64%"=="Y" (
+	call "%SELF%\test.bat" x64 14.3.0
 	call "%SELF%\test.bat" x64 13.0.1
 	call "%SELF%\test.bat" x64 12.13.0
 	call "%SELF%\test.bat" x64 11.3.0
