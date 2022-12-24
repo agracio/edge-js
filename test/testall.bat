d2dc0428b1992873804791acb65eca64be4a9fa5
agracio <antonas.graciovas@gmail.com>
2022-12-24 12:27:09 +0000

    adding support for 19.3.0

--- test/testall.bat
+++ test/testall.bat
@@ -10,12 +10,14 @@ if "%1"=="" set run64=Y
 if "%1"=="x64" set run64=Y
 
 if "%run32%"=="Y" (
+	call "%SELF%\test.bat" ia32 19.3.0
 	call "%SELF%\test.bat" ia32 18.4.0
 	call "%SELF%\test.bat" ia32 16.15.1
 	call "%SELF%\test.bat" ia32 14.19.3
 )
 
 if "%run64%"=="Y" (
+	call "%SELF%\test.bat" x64 19.3.0
 	call "%SELF%\test.bat" x64 18.4.0
 	call "%SELF%\test.bat" x64 16.15.1
 	call "%SELF%\test.bat" x64 14.19.3
--- tools/buildall.bat
+++ tools/buildall.bat
@@ -1,2 +1,2 @@
 @echo off
-"%~dp0\build.bat" release 14.19.3 16.15.1 18.4.0
+"%~dp0\build.bat" release 14.19.3 16.15.1 18.4.0 19.3.0
