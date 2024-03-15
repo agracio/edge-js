a32bdc305d3228193d72ec001cf83e978aef5677
agracio <antonas.graciovas@gmail.com>
2024-03-15 15:11:49 +0000

    updating CI

--- appveyor.ps1
+++ appveyor.ps1
@@ -11,11 +11,11 @@ Write-Host "$(Get-TimeStamp) Downloading latest .NET Core SDK..."
 
 (New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/9e753d68-7701-4ddf-b358-79d64e776945/2a58564c6d0779a7b443a692c520782f/dotnet-sdk-8.0.203-win-x64.exe','dotnet-core-sdk.exe')
 
-ls
+ls C:\projects
 
 Write-Host "$(Get-TimeStamp) Installing .NET Core SDK..."
 
-Start-Process -Wait 'C:\dotnet-core-sdk.exe' -ArgumentList '/install /quiet /norestart';
+Start-Process -Wait 'C:\projects\dotnet-core-sdk.exe' -ArgumentList '/install /quiet /norestart';
 
 # Invoke-Command -ScriptBlock { dotnet-core-sdk.exe /install /quiet /norestart }
 
