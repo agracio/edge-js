c34aac57d890ec7d736fd2d83475a1cb359bd15d
agracio <antonas.graciovas@gmail.com>
2024-05-01 18:00:57 +0100

    updating ci config

--- tools/test.js
+++ tools/test.js
@@ -17,7 +17,7 @@ if (!process.env.EDGE_USE_CORECLR) {
 		buildParameters = buildParameters.concat(['-sdk:4.5']);
 	}
 
-	run(process.platform === 'win32' ? 'csc' : 'mcs', buildParameters, runOnSuccess);
+	run(process.platform === 'win32' ? 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\csc.exe' : 'mcs', buildParameters, runOnSuccess);
 }
 
 else {
