3b7c19fa14c4b08dedfd00df7cb8d9aad41c8355
agracio <antonas.graciovas@outlook.com>
2024-06-06 19:21:05 +0100

    updating build tools

--- tools/build.bat
+++ tools/build.bat
@@ -24,10 +24,16 @@ if "%1" neq "" (
     shift
     goto :harvestVersions
 )
-if "%VERSIONS%" equ "" set VERSIONS=0.10.0
+if "%VERSIONS%" equ "" set VERSIONS=20.14.0
 pushd %SELF%\..
-for %%V in (%VERSIONS%) do call :build ia32 x86 %%V 
-for %%V in (%VERSIONS%) do call :build x64 x64 %%V 
+
+if "%PROCESSOR_ARCHITECTURE%" == "ARM64" (
+    for %%V in (%VERSIONS%) do call :build arm64 arm64 %%V 
+) else (
+    for %%V in (%VERSIONS%) do call :build ia32 x86 %%V 
+    for %%V in (%VERSIONS%) do call :build x64 x64 %%V 
+
+)
 popd
 
 exit /b 0
@@ -35,6 +41,7 @@ exit /b 0
 :build
 
 set DESTDIR=%DESTDIRROOT%\%1\%3
+
 if exist "%DESTDIR%\node.exe" goto gyp
 if not exist "%DESTDIR%\NUL" mkdir "%DESTDIR%"
 echo Downloading node.exe %2 %3...
@@ -56,11 +63,21 @@ if not exist "%GYP%" (
     exit /b -1
 )
 
-"%NODEEXE%" "%GYP%" configure build --msvs_version=2019 -%FLAVOR%
+"%NODEEXE%" "%GYP%" configure --msvs_version=2022 -%FLAVOR%
 if %ERRORLEVEL% neq 0 (
     echo Error building edge.node %FLAVOR% for node.js %2 v%3
     exit /b -1
 )
+
+REM Conflict when building arm64 binaries
+if "%PROCESSOR_ARCHITECTURE%" == "ARM64" (
+    FOR %%F IN (build\*.vcxproj) DO (
+    echo Patch /fp:strict in %%F
+    powershell -Command "(Get-Content -Raw %%F) -replace '<FloatingPointModel>Strict</FloatingPointModel>', '<!-- <FloatingPointModel>Strict</FloatingPointModel> -->' | Out-File -Encoding Utf8 %%F"
+    )
+)
+
+"%NODEEXE%" "%GYP%" build
 
 echo %DESTDIR%
 copy /y .\build\%FLAVOR%\edge_*.node "%DESTDIR%"
--- tools/test.js
+++ tools/test.js
@@ -25,7 +25,7 @@ if (!process.env.EDGE_USE_CORECLR) {
 		buildParameters = buildParameters.concat(['-sdk:4.5']);
 	}
 
-    var compiler = runner === 'circleci' || runner === 'CI' ? 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\csc.exe' : 'csc'
+  var compiler = 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\csc.exe'
 
 	run(process.platform === 'win32' ? compiler : 'mcs', buildParameters, runOnSuccess);
 }
