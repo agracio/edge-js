52ccb8a20800a38f939e45ad68e9b3afbf05f5c6
agracio <agracio@users.noreply.github.com>
2025-04-24 00:40:24 +0100

    updating mono check

--- tools/checkMono.js
+++ tools/checkMono.js
@@ -1,12 +1,21 @@
-const { exec, spawn, spawnSync } = require('child_process');
+const { exec } = require('child_process');
 
-function exists(cmd){
+function exists(cmd, cb){
     exec(cmd, function (err, stdout, stderr) {
-        return stdout.toLowerCase().includes('not found') ? false : true;
+        let result
+        if(err){
+            result = false;
+        }
+        else{
+            result = stdout.toLowerCase().includes('not found') || stdout.length === 0 ? false : true
+        }
+        cb(result);
     });
 }
 
-let mono = exists('which mono');
-let pkgconfig = exists('which pkg-config');
-return mono && pkgconfig;
+exists('which mono', (mono)=>{
+    exists('which pkg-config', (pkgconfig)=>{
+        console.log(mono && pkgconfig)
+    });
+});
 
