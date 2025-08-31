20408fbef05bde1a40fd00bd6dd404849f54b79f
agracio <agracio@users.noreply.github.com>
2025-08-31 23:38:00 +0100

    updating CI

--- tools/getVersion.js
+++ tools/getVersion.js
@@ -1,38 +1,27 @@
-var {http} = require('follow-redirects');
+const fs = require("fs");
+const { execSync } = require('child_process');
 
-try_get();
+getVersion();
 
-function try_get() {
-	let url = 'http://nodejs.org/dist/index.json';
+function getVersion() {
 
-	http.get(url,(res) => {
-		let body = "";
+    function getVersionFromMajor(json, major){
+        const version = json.find((str) =>  str.startsWith(`${major}.`));
+        if(version) return version; 
+        throw new Error(`Unable to resolve latest version for Node.js ${major}`);
+    }
 
-		if (res.statusCode !== 200) {
-			throw new Error(`Unable to get Node.js versions from ${url}`);
-		}
-	
-		res.on("data", (chunk) => {
-			body += chunk;
-		});
-	
-		res.on("end", () => {
-			try {
-				let json = JSON.parse(body);
+    let json = JSON.parse(execSync('npm view node versions --json').toString())
+    .reverse();
 
-				for (const el of json.sort()) {
-					let version = el.version.substring(1, el.version.length) ;
-					if(version.startsWith(process.argv[2])){
-						console.log(version)
-						return version;
-					}
-				}
-			} catch (error) {
-				throw error;
-			};
-		});
-	
-	}).on("error", (error) => {
-		throw new Error(error);
-	});
+    let major = process.argv[2];
+    if(major){
+        version = getVersionFromMajor(json, major);
+        fs.writeFileSync('node.txt', version);
+		return version;
+    }
+	else{
+		throw new Error('No Node.js version provided');
+	}
 }
+
